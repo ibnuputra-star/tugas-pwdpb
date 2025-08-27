98f7ed0002838f7e02c89955d1209c58286c2805
ibnuputra-star <ibnuputra196@gmail.com>
2025-08-27 22:51:15 +0700

    Add files via upload

---
+++ 0096381510_ibnuputrabatuta_XI.PPLG.1.tugasmodul01-php/lat6.php
@@ -0,0 +1,13 @@
+<?php
+$a = 10;
+$b = 5;
+$hasilTambah = $a + $b;
+$hasilKurang =$a - $b;
+$hasilKali =$a * $b;
+$hasilBagi =$a / $b;
+
+echo $hasilTambah;  // 15
+echo $hasilKurang;  // 5 
+echo $hasilKali;  // 50 
+echo $hasilBagi;  // 2
+?>
